250f8a42ffca6b52a4a298ee50fcd23964a36c16
duthienkt <blueskythien2010@live.com>
2021-05-10 21:28:22 +0700

    + TDEEnumSet

---
+++ js/fragment/editor/TDEEnumSet.js
@@ -0,0 +1,42 @@
+import OOP from "absol/src/HTML5/OOP";
+import TDEBase from "./TDEBase";
+import ResizeSystem from "absol/src/HTML5/ResizeSystem";
+
+/***
+ * @extends TDEBase
+ * @constructor
+ */
+function TDEEnumSet() {
+    TDEBase.apply(this, arguments);
+}
+
+OOP.mixClass(TDEEnumSet, TDEBase);
+
+
+TDEEnumSet.prototype.prepareInput = function () {
+    var descriptor = this.cell.descriptor;
+    this.$input = _({
+        tag: 'multiselectmenu',
+        class: 'asht-enum-set-cell-editor-input',
+        props: {
+            items: descriptor.items,
+            values: this.cell.value
+        },
+        on: {
+            change: this.ev_inputChange
+        }
+    });
+    this.$editingbox.clearChild()
+        .addChild(this.$input);
+};
+
+
+TDEEnumSet.prototype.ev_inputChange = function () {
+    this.cell.value = this.$input.values;
+    ResizeSystem.update();
+};
+
+TDEBase.typeClasses['{enum}'] = TDEEnumSet;
+TDEBase.typeClasses.NumSet = TDEEnumSet;
+
+export default TDEEnumSet;
