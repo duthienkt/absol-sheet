e79a91f52058c13d34bf606039b5e7bbf8514c55
duthienkt <blueskythien2010@live.com>
2021-05-19 19:27:26 +0700

    - number

--- js/fragment/editor/TDENumber.js
+++ js/fragment/editor/TDENumber.js
@@ -1,4 +1,3 @@
-import PreInput from "absol-acomp/js/PreInput";
 import OOP from "absol/src/HTML5/OOP";
 import TDEBase from "./TDEBase";
 import TDEText from "./TDEText";
@@ -18,7 +17,6 @@ function TDENumber(tableEditor, cell) {
 OOP.mixClass(TDENumber, TDEText);
 
 TDENumber.prototype.prepareInput = function () {
-    var descriptor = this.cell.descriptor;
     /***
      * @type {PreInput}
      */
@@ -29,113 +27,57 @@ TDENumber.prototype.prepareInput = function () {
         },
         class: 'asht-cell-editor-input',
         on: {
-            blur: this.ev_blur
+            change: this.ev_inputChange,
+            keydown: this.ev_keydown
         }
     });
-    if (descriptor.step) this.$input.step = descriptor.step;
     this.$editingbox.clearChild()
         .addChild(this.$input);
+    this._loadCellStyle();
+    this.$input.addStyle(this._cellStyle);
 };
 
+TDENumber.prototype._loadCellStyle = function () {
+    var cellElt = this.cell.elt;
+    this._cellStyle = {
+        'font-size': cellElt.getComputedStyleValue('font-size'),
+        'font-family': cellElt.getComputedStyleValue('font-family'),
+        'font-style': cellElt.getComputedStyleValue('font-style'),
+        'line-height': cellElt.getComputedStyleValue('line-height'),
+        'padding-left': cellElt.getComputedStyleValue('padding-left'),
+        'padding-top': cellElt.getComputedStyleValue('padding-top'),
+        'padding-bottom': cellElt.getComputedStyleValue('padding-bottom'),
+        'text-align': cellElt.getComputedStyleValue('text-align'),
+    };
+};
 
-TDENumber.prototype.waitAction = function () {
-    TDEBase.prototype.waitAction.call(this);
+TDENumber.prototype.load = function () {
+    var descriptor = this.cell.descriptor;
+    if (descriptor.step) this.$input.step = descriptor.step;
+    this.$input.value = this.cell.value;
+};
+
+TDENumber.prototype.onStart = function (){
     setTimeout(function () {
         this.$input.focus();
         this.$input.value = this.cell.value;
         this.$input.select();
-    }.bind(this), 100);
-    this.$input.removeClass('asht-state-editing')
-        .addClass('asht-state-wait-action');
-    this.$input.on('keydown', this.ev_firstKey)
-        .off('keydown', this.ev_finishKey)
-        .on('dblclick', this.ev_dblClick);
-};
-
-TDENumber.prototype.startEditing = function () {
-    TDEBase.prototype.startEditing.call(this);
-    this.$input.addClass('asht-state-editing')
-        .removeClass('asht-state-wait-action');
-    this.$input.off('keydown', this.ev_firstKey)
-        .off('dblclick', this.ev_dblClick);
-    this.$input.on('keydown', this.ev_finishKey);
+    }.bind(this), 5);
 };
 
 
-/***
- *
- * @param {KeyboardEvent} event
- */
-TDENumber.prototype.ev_firstKey = function (event) {
-    if (event.key === "Delete") {
-        this.cell.value = 0;
-    }
-    else if (event.key === 'Enter' || event.key === 'F2') {
-        this.$input.value = this.cell.value;
-        this.startEditing();
-        event.preventDefault();
-    }
-    else if (event.key === 'Tab') {
-        this.editCellNext();
-        event.preventDefault();
-    }
-    else if (event.key.length === 1 || event.key === "Backspace") {
-        this.startEditing();
-    }
-    else if (event.key.startsWith('Arrow')) {
-        event.preventDefault();
-        switch (event.key) {
-            case "ArrowLeft":
-                this.editCellLeft();
-                break;
-            case "ArrowRight":
-                this.editCellRight();
-                break;
-            case "ArrowUp":
-                this.editCellAbove();
-                break;
-            case "ArrowDown":
-                this.editCellBellow();
-                break;
-        }
-    }
-};
 
-TDENumber.prototype.ev_finishKey = function (event) {
-    if (event.key === "Enter"|| event.key === "Tab") {
+
+
+TDENumber.prototype.ev_keydown = function (event) {
+    if (event.key === "Enter" || event.key === "Tab") {
         var text = this.$input.value;
         this.cell.value = parseFloat(text) || this.cell.value;
         this.tableEditor.updateFixedTableEltPosition();
         event.preventDefault();
         this.editCellNext();
     }
-    else if (event.key === "Escape") {
-        event.preventDefault();
-        this.waitAction();
-    }
 };
-
-TDENumber.prototype.ev_dblClick = function (event) {
-    event.preventDefault();
-    this.$input.value = this.cell.value;
-    setTimeout(this.$input.focus.bind(this.$input), 100);
-    this.startEditing();
-};
-
-
-TDENumber.prototype.ev_blur = function (event) {
-    this.$editingbox.removeClass('as-status-focus');
-    setTimeout(function () {
-        if (this.$input !== document.activeElement) {
-            //blur before finished
-            var value = parseFloat(this.$input.value);
-            if (isFinite(value))
-                this.cell.value = value;
-        }
-    }.bind(this), 100);
-};
-
-TDENumber.prototype.ev_focus = TDEText.prototype.ev_focus;
 
 TDEBase.typeClasses.number = TDENumber;
 TDEBase.typeClasses.Number = TDENumber;
