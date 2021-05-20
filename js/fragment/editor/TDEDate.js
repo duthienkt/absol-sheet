8057a84d585a4d5fb99030fff4246d28621a65ae
duthienkt <blueskythien2010@live.com>
2021-05-20 21:14:48 +0700

    - load date value

--- js/fragment/editor/TDEDate.js
+++ js/fragment/editor/TDEDate.js
@@ -5,6 +5,7 @@ import SelectMenu from "absol-acomp/js/SelectMenu2";
 import AElement from "absol/src/HTML5/AElement";
 import {_, $} from '../../dom/SCore';
 import TDEText from "./TDEText";
+import {parseDateString} from "absol/src/Time/datetime";
 
 
 /***
@@ -33,6 +34,23 @@ TDEDate.prototype.prepareInput = function () {
     this.$input.addStyle(this._cellStyle);
 };
 
+
+TDEDate.prototype.reload = function (){
+    var value = this.cell.value;
+    var descriptor = this.cell.descriptor;
+    var dateValue;
+    if (typeof value === "string"){
+        dateValue = new Date(value);
+        if (isNaN(dateValue.getTime())){
+            dateValue = parseDateString(value, descriptor.format || 'dd/mm/yyyy');
+        }
+        if (isNaN(dateValue.getTime())){
+            dateValue = null;
+        }
+    }
+    this.$input.value = dateValue;
+};
+
 TDEDate.prototype._loadCellStyle = function () {
     var cellElt = this.cell.elt;
     this._cellStyle = {
@@ -40,110 +58,6 @@ TDEDate.prototype._loadCellStyle = function () {
         'font-family': cellElt.getComputedStyleValue('font-family'),
         'font-style': cellElt.getComputedStyleValue('font-style')
     };
-};
-
-TDEDate.prototype.waitAction = function () {
-    TDEBase.prototype.waitAction.call(this);
-    setTimeout(function () {
-        this.$input.$input.focus();
-    }.bind(this), 100);
-    this.$input.removeClass('asht-state-editing')
-        .addClass('asht-state-wait-action');
-    this.$input.$input.on('keydown', this.ev_firstKey)
-        .off('keydown', this.ev_finishKey)
-        .on('dblclick', this.ev_dblClick);
-};
-
-TDEDate.prototype.startEditing = function () {
-    TDEBase.prototype.startEditing.call(this);
-    this.$input.addClass('asht-state-editing')
-        .removeClass('asht-state-wait-action');
-    this.$input.$input.off('keydown', this.ev_firstKey)
-        .off('dblclick', this.ev_dblClick);
-    this.$input.value = this.cell.dateValue;
-    setTimeout(function () {
-        this.$input.$input.focus();
-        this.$input.$input.select();
-    }.bind(this), 10);
-    this.$input.$input.on('keydown', this.ev_finishKey);
-};
-
-TDEDate.prototype.finish = function () {
-    if (this.state === "FINISHED") return;
-    if (this._waitBlurTimeout > 0)
-        clearTimeout(this._waitBlurTimeout);
-    this.$input.$input.off('keydown', this.ev_finishKey)
-        .off('keydown', this.ev_firstKey)
-        .off('dblclick', this.ev_dblClick)
-        .off('blur', this.ev_blur);
-    TDEBase.prototype.finish.call(this);
-};
-
-
-/***
- *
- * @param {KeyboardEvent} event
- */
-TDEDate.prototype.ev_firstKey = function (event) {
-    if (event.key === "Delete") {
-        this.cell.value = "";
-    }
-    else if (event.key === 'Enter' || event.key === 'F2') {
-        this.$input.value = this.cell.value;
-        this.startEditing();
-        event.preventDefault();
-    }
-    else if (event.key === 'Tab') {
-        this.editCellNext();
-        event.preventDefault();
-    }
-    else if (event.key.length === 1 || event.key === "Backspace") {
-        this.startEditing();
-    }
-    else if (event.key.startsWith('Arrow')) {
-        event.preventDefault();
-        switch (event.key) {
-            case "ArrowLeft":
-                this.editCellLeft();
-                break;
-            case "ArrowRight":
-                this.editCellRight();
-                break;
-            case "ArrowUp":
-                this.editCellAbove();
-                break;
-            case "ArrowDown":
-                this.editCellBellow();
-                break;
-        }
-    }
-};
-
-TDEDate.prototype.ev_finishKey = function (event) {
-    if (event.key === "Tab") {
-        this.editCellNext();
-        event.preventDefault();
-    }
-};
-
-TDEDate.prototype.ev_dblClick = function (event) {
-    event.preventDefault();
-    this.startEditing();
-};
-
-// TODO: handle enter key, blur
-
-TDEDate.prototype.ev_blur = function (event) {
-    this.$editingbox.removeClass('as-status-focus');
-    if (this._waitBlurTimeout >= 0) clearTimeout(this._waitBlurTimeout);
-    this._waitBlurTimeout = setTimeout(function () {
-        this._waitBlurTimeout = -1;
-        if (!document.activeElement
-            || (this.$input.$input !== document.activeElement
-                && !AElement.prototype.isDescendantOf.call(document.activeElement, this.$input))) {
-            //blur before finished
-        }
-    }.bind(this), 100);
 };
 
 TDEDate.prototype.ev_inputChange = function () {
