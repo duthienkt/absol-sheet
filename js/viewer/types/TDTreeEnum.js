a463213e34f51a843e4654beea09050317ab0c49
duthienkt <blueskythien2010@live.com>
2021-05-29 20:23:22 +0700

    - measure size, implicit

--- js/viewer/types/TDTreeEnum.js
+++ js/viewer/types/TDTreeEnum.js
@@ -1,6 +1,9 @@
 import TDBase from "./TDBase";
 import OOP from "absol/src/HTML5/OOP";
 import {_} from "../../dom/SCore";
+import {measureListSize} from "absol-acomp/js/SelectList";
+import treeListToList from "absol-acomp/js/list/treeListToList";
+import TDEnum from "./TDEnum";
 
 
 /***
@@ -22,13 +25,14 @@ TDTreeEnum.prototype.attachView = function () {
     this.elt.addChild(this.$text);
 };
 
-TDTreeEnum.prototype.reload = function () {
+TDTreeEnum.prototype.loadDescriptor = function () {
     var descriptor = this.descriptor;
+    descriptor.items = descriptor.items || [];
     if (!descriptor.__val2Item__) {
-        Object.defineProperty(descriptor, '__val2Item__', {
+        Object.defineProperty(descriptor.items, '__val2Item__', {
             configurable: true,
             enumerable: false,
-            value: (descriptor.items || [])
+            value: descriptor.items
                 .reduce(function visitor(ac, item) {
                     ac[item.value] = item;
                     if (item.items && item.items.length > 0)
@@ -36,15 +40,33 @@ TDTreeEnum.prototype.reload = function () {
                     return ac;
                 }, {})
         });
+        var listSize = measureListSize(treeListToList(descriptor.items || []));
+        Object.defineProperty(descriptor.items, '__width14__', {
+            configurable: true,
+            enumerable: false,
+            value: listSize.width
+        });
     }
+    this.elt.addStyle('min-width', (descriptor.items.__width14__ + 50) / 14 + 'em');
+    var value = this.record[this.pName];
+    if (value !== null && value !== undefined) {
+        this.record[this.pName] = descriptor.items.length > 0 ? descriptor.items[0].value : null;
+    }
+};
+
+TDTreeEnum.prototype.implicit = TDEnum.prototype.implicit;
+
+TDTreeEnum.prototype.loadValue = function () {
+    var descriptor = this.descriptor;
     var value = this.value;
-    if (value !== null && value !== undefined && descriptor.__val2Item__[value]) {
-        this.$text.firstChild.data = descriptor.__val2Item__[value].text;
+    if (value !== null && value !== undefined && descriptor.items.__val2Item__[value]) {
+        this.$text.firstChild.data = descriptor.items.__val2Item__[value].text;
     }
     else {
         this.$text.firstChild.data = '';
     }
 };
+
 
 TDBase.typeClasses.treeenum = TDTreeEnum;
 TDBase.typeClasses.TreeEnum = TDTreeEnum;
