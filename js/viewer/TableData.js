a0511deafedca141226b388db54071b51f0739c9
duthienkt <blueskythien2010@live.com>
2021-05-29 20:24:13 +0700

    - new row, config row height

--- js/viewer/TableData.js
+++ js/viewer/TableData.js
@@ -1,20 +1,56 @@
 import SComp from '../dom/SComp';
 import TDRecord from "./TDRecord";
+import Attributes from "absol/src/AppPattern/Attributes";
+import '../../css/tabledata.css';
+import EventEmitter from "absol/src/HTML5/EventEmitter";
+import OOP from "absol/src/HTML5/OOP";
+import ResizeSystem from "absol/src/HTML5/ResizeSystem";
 
 var _ = SComp._;
 var $ = SComp.$;
 
 
 /**
- *
+ * @extends EventEmitter
  * @constructor
  */
 function TableData() {
+    EventEmitter.call(this);
     this.propertyNames = [];
     this.propertyDescriptors = {};
     this.records = [];
     this.bodyRow = [];
     this.headCells = [];
+    this.newRow = null;
+    /***
+     *
+     * @type {Attributes}
+     */
+    this.config = new Attributes(this);
+    this.config.loadAttributeHandlers(this.configHandlers);
+    for (var key in this){
+        if (key.startsWith('ev_')){
+            this[key] = this[key].bind(this);
+        }
+    }
+}
+
+OOP.mixClass(TableData, EventEmitter);
+
+TableData.prototype.defaultConfig = {
+    rowHeight: 21
+};
+
+TableData.prototype.configHandlers = {};
+
+TableData.prototype.configHandlers.rowHeight = {
+    set: function (value) {
+        if (!(value > 0 && value < 1024)) {
+            value = this.defaultConfig.rowHeight;
+        }
+        this.$view.addStyle('--row-height', value + 'px');
+        return value;
+    }
 }
 
 
@@ -22,7 +58,8 @@ TableData.prototype.export = function () {
     return {
         propertyNames: this.propertyNames,
         propertyDescriptors: this.propertyDescriptors,
-        records: this.records
+        records: this.records,
+        config: this.config.export()
     }
 };
 
@@ -30,6 +67,7 @@ TableData.prototype.import = function (data) {
     this.propertyDescriptors = data.propertyDescriptors;
     this.propertyNames = data.propertyNames;
     this.records = data.records;
+    Object.assign(this.config, this.defaultConfig, data.config || {});
     this._computeData();
     this.reload();
 };
@@ -139,6 +177,9 @@ TableData.prototype.loadBody = function () {
         return row.elt;
     });
     this.$tbody.addChild(rowEltList);
+    this.newRow = new TDRecord(this, {}, '*');
+    this.newRow.on('property_change', this.ev_newRowPropertyChange);
+    this.$tbody.addChild(this.newRow.elt);
 };
 
 
@@ -161,17 +202,22 @@ TableData.prototype.getView = function () {
     this.$headRow = $('tr', this.$thead);
     this.$tbody = $('tbody', this.$view);
     this.$rootCell = _('td.asht-table-data-root-cell');
-    this.$newRow = _({
-        tag: 'tr',
-        class: 'asht-row-new',
-        child: [{
-            tag: 'td',
-            child: {
-                text: "*"
-            }
-        }]
-    })
     return this.$view;
+};
+
+/***
+ *
+ * @param {Object=} newRecord
+ */
+TableData.prototype.flushNewRow = function (newRecord) {
+    this.newRow.idx = this.records.length;
+    this.newRow.off('property_change', this.ev_newRowPropertyChange);
+    this.records.push(this.newRow.record);
+    this.bodyRow.push(this.newRow);
+    this.newRow = new TDRecord(this, newRecord || {}, "*");
+    this.newRow.on('property_change', this.ev_newRowPropertyChange);
+    this.$tbody.addChild(this.newRow.elt);
+    ResizeSystem.update();
 };
 
 
@@ -179,7 +225,7 @@ TableData.prototype.getView = function () {
  * @param {Number} y
  */
 TableData.prototype.findRowByClientY = function (y) {
-    var length = this.bodyRow.length;
+    var length = this.bodyRow.length + 1;
     var start = 0;
     var mid;
     var row;
@@ -187,7 +233,7 @@ TableData.prototype.findRowByClientY = function (y) {
     var position;
     while (length > 0) {
         mid = start + (length >> 1);
-        row = this.bodyRow[mid];
+        row = this.bodyRow[mid] || this.newRow;
         position = row.elt.getBoundingClientRect();
         rowY = position.top;
         if (y < rowY) {
@@ -244,15 +290,21 @@ TableData.prototype.findColByName = function (name) {
 
 
 TableData.prototype.findRowByIndex = function (index) {
+    if (index === '*') return this.newRow;
     return this.bodyRow[index] || null;
 };
 
-TableData.prototype.findIndexOfRow = function (row) {
-    return this.bodyRow.indexOf(row);
-};
 
 TableData.prototype.findIndexOfCol = function (col) {
     return this.headCells.indexOf(col);
+};
+
+TableData.prototype.ev_newRowPropertyChange = function (event) {
+    this.emit('new_row_property_change', Object.assign({}, event, {
+        type: 'new_row_property_change',
+        target: this,
+        tdRecord: event.target
+    }), this);
 };
 
 
