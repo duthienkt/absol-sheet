0d8a3f7ef51bda27d3abdfb5d4008a6d98711ded
duthienkt <blueskythien2010@live.com>
2021-07-09 20:45:23 +0700

    + TSFunction

---
+++ js/fx/TSFunction.js
@@ -0,0 +1,106 @@
+var types = babel.types;
+
+function TSFunction(propertyNames, body) {
+    this.propertyNames = propertyNames;
+    this.body = body;
+    this.dependents = [];
+    this.ast = null;
+    this.func = null;
+    this._compile();
+}
+
+
+TSFunction.prototype.localConstants = {
+    MAX: Math.max.bind(Math)
+};
+
+TSFunction.prototype._isAsync = function (jsCode) {
+    if (!window.babel) return false;
+    var scriptCode = 'async function fx(){\n'
+        + jsCode
+        + '\n}';
+    var result = false;
+    var ast = babel.parseSync(scriptCode, {});
+    babel.traverse(ast, {
+        AwaitExpression: function () {
+            console.log("AW")
+            result = true;
+        }
+    });
+    return result;
+};
+
+
+TSFunction.prototype._makeConstCode = function (localConstants) {
+    return Object.keys(localConstants).map(function (key) {
+        return 'const ' + key + ' = localConstants[' + JSON.stringify(key) + '];'
+    }).join('\n') + '\n';
+};
+
+TSFunction.prototype._compile = function () {
+    var scriptCode;
+    if (this.body.startsWith('=')) {
+        scriptCode = 'RET' + this.body;
+    }
+    else scriptCode = this.body;
+    if (!window.babel) return;
+    var variableDict = this.propertyNames.reduce(function (ac, cr) {
+        ac[cr] = true;
+        return ac;
+    }, {});
+    try {
+        var isAsync = this._isAsync(scriptCode);
+        scriptCode = (isAsync ? 'async ' : '') + 'function fx(record){\n'
+            + 'var RET;\n'
+            + scriptCode
+            + '\nreturn RET;'
+            + '\n}';
+        this.ast = babel.parseSync(scriptCode, {});
+        var variables = {};
+        var asyncFlag = '';
+        babel.traverse(this.ast, {
+            Program: function (path) {
+            },
+            Identifier: function (path) {
+                var node = path.node;
+                var name = node.name;
+                if (path.container && path.container.length) return;
+                if (path.container.type === "MemberExpression" && path.container.object !== node) return;
+                var newNode;
+                if (variableDict[name]) {
+                    variables[name] = true;
+                    newNode = types.memberExpression(
+                        types.identifier('record'),
+                        types.identifier(name)
+                    );
+                    newNode.ignore = true;
+                    path.replaceWith(newNode);
+
+                }
+            },
+            MemberExpression: function (path) {
+                if (path.node.ignore) path.skip();
+            }
+        });
+
+        this.dependents = Object.keys(variables);
+        this.jsCode = 'module.exports = ' + babel.generate(this.ast).code;
+        var options = {
+            presets: [
+                babel.presetEnv
+            ]
+        };
+        this.transformedCode = babel.transform(this.jsCode, options).code;
+        var mdl = {};
+        (new Function('module', 'regeneratorRuntime', 'localConstants', this._makeConstCode(this.localConstants) + this.transformedCode))(mdl, babel.regeneratorRuntime, this.localConstants);
+        this.func = mdl.exports;
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+TSFunction.prototype.invoke = function (_this, record) {
+    return this.func.call(this, record);
+};
+
+export default TSFunction;
