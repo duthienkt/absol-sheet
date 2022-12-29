2038b21cb4a68f6280665374ea5deee4b4f05a4d
duthienkt <blueskythien2010@live.com>
2022-12-29 21:02:55 +0700

    - export

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import SComp from "./js/dom/SComp";
 import TableEditor from "./js/fragment/TableEditor";
 import TSFunction from "./js/fx/TSFunction";
-
+import * as ExcelFx from './js/fx/ExcelFx';
 
 export default {
     dom: SComp,
     TableEditor: TableEditor,
-    TSFunction: TSFunction
+    TSFunction: TSFunction,
+    ExcelFx: ExcelFx
 };
