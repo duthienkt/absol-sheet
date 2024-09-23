823d8d848eafb98b8c49b1fd975fc73b19f12280
duthienkt <blueskythien2010@live.com>
2024-09-23 11:24:24 +0700

    - export

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-import SComp from "./js/dom/SComp";
 import TableEditor from "./js/fragment/TableEditor";
 import TSFunction from "./js/fx/TSFunction";
 import * as ExcelFx from './js/fx/ExcelFx';
 import './js/fx/ExcelParser';
 import { excelFormula2KVFormula } from "./js/fx/ExcelParser";
+import FormArrayEditor from "./js/fragment/FormArrayEditor";
+import SCore from "./js/dom/SCore";
 
 export default {
-    dom: SComp,
+    dom: SCore,
     TableEditor: TableEditor,
+    FormArrayEditor: FormArrayEditor,
     TSFunction: TSFunction,
     ExcelFx: ExcelFx,
     excelFormula2KVFormula: excelFormula2KVFormula
