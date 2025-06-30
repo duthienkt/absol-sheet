15c19c2dfd5aad5cf5966ba05574d6eef2b94ae6
duthienkt <blueskythien2010@live.com>
2025-06-30 20:24:56 +0700

    - support ez-form format

--- js/fragment/Abstractions.js
+++ js/fragment/Abstractions.js
@@ -1,16 +1,21 @@
-import OOP from "absol/src/HTML5/OOP";
+import OOP, { mixClass } from "absol/src/HTML5/OOP";
 import Fragment from "absol/src/AppPattern/Fragment";
 import EventEmitter from "absol/src/HTML5/EventEmitter";
 import Attributes from "absol/src/AppPattern/Attributes";
 import { copyJSVariable } from "absol/src/JSMaker/generator";
 import { _ } from "../dom/SCore";
-import { keyStringOf } from "absol-acomp/js/utils";
+import { isRealNumber, keyStringOf } from "absol-acomp/js/utils";
 import ResizeSystem from "absol/src/HTML5/ResizeSystem";
 import noop from "absol/src/Code/noop";
 import Context from "absol/src/AppPattern/Context";
 import { randomIdent } from "absol/src/String/stringGenerate";
 import { computeSheetDescriptor } from "../util";
 import { stringHashCode } from "absol/src/String/stringUtils";
+import SCScope from "absol/src/SCLang/SCScope";
+import CCBlock from "absol/src/AppPattern/circuit/CCBlock";
+import CBFunction from "../fx/CBFunction";
+import CCLine from "absol/src/AppPattern/circuit/CCLine";
+import TDBase from "../viewer/types/TDBase";
 
 
 /**
@@ -24,7 +29,13 @@ export function ASHTEditor(opt) {
     this.opt = new Attributes(this);
     Object.assign(this.opt, copyJSVariable(this.defaultOpt), opt);
     this.tableData = null;
-
+    this.varialbeScope = new SCScope();
+    this.setContext('variableScope', this.varialbeScope);
+    if (opt.context) {
+        Object.keys(opt.context).forEach(key => {
+            this.varialbeScope.declareConst(key, opt.context[key]);
+        });
+    }
 }
 
 OOP.mixClass(ASHTEditor, Fragment, EventEmitter);
@@ -78,9 +89,12 @@ Object.defineProperty(ASHTEditor.prototype, 'fragment', {
  * @constructor
  */
 export function ASHField(row, pName) {
+    CCBlock.call(this);
     this.elt = _('td');
     this.row = row;
     this.pName = pName;
+    this.isExtraField = false;//if true, this field is in record.extrainfo
+    this.pId = null;//column id, use in block/line
     this.sync = this.renewDescriptor();
     if (this.sync) {
         this.sync = this.sync.then(() => {
@@ -96,11 +110,22 @@ export function ASHField(row, pName) {
     }
 }
 
+mixClass(ASHField, CCBlock);
+
+ASHField.prototype.getVariablesContext = function () {
+    return this.table.getVariablesContext();
+};
+
 
 ASHField.prototype.renewDescriptor = function () {
     var self = this;
     var originDescriptor = this.row.table.propertyDescriptors && this.row.table.propertyDescriptors[this.pName];
-    var descriptor = { type: 'text' };
+    var descriptor = {type: 'text'};
+    if (originDescriptor && originDescriptor.pId) {
+        this.pId = originDescriptor.pId;
+    }
+    if (originDescriptor.isExtraField) this.isExtraField = true;
+
 
     Object.keys(originDescriptor).forEach(key => {
         var assigned = false;
@@ -130,7 +155,7 @@ ASHField.prototype.renewDescriptor = function () {
                 Object.assign(descriptor, fx[key].getCase(self.record));
             }
             else {
-                ac[key] = fx[key].invoke(self, self.record);
+                ac[key] = fx[key].invoke(self, self.record, self.getVariablesContext());
                 if (ac[key] && ac[key].then) {
                     ac[key] = ac[key].then(function (result) {
                         ac[key] = result;
@@ -170,7 +195,7 @@ ASHField.prototype.isEmpty = function () {
 
 ASHField.prototype.isNoneValue = function (value) {
     var descriptor = this.descriptor;
-    return value === null || value === undefined || ((typeof value === "number") && isNaN(value)) || value === "" || value === descriptor.emptyValue;
+    return value === null || value === undefined || ((typeof value === "number") && !isRealNumber(value)) || value === "" || value === descriptor.emptyValue;
 };
 
 
@@ -183,18 +208,24 @@ ASHField.prototype.makeDefaultValue = function () {
 };
 
 ASHField.prototype.attachView = function () {
-    this.$text = _({ text: '?[' + JSON.stringify(this.value) + ']' });
+    this.$text = _({text: '?[' + JSON.stringify(this.value) + ']'});
     this.elt.addChild(this.$text);
 };
 
 
 ASHField.prototype.execOnChange = function () {
     var self = this;
-    var record = this.record;
-    var newRecord = Object.assign({}, this.record);
+    var record = Object.assign({}, this.record);
+    var extrainfo = record.extrainfo;
+    if (extrainfo && (extrainfo === 'object')) {
+        delete record.extrainfo;
+        Object.assign(record, extrainfo);
+    }
+
+    var newRecord = Object.assign({}, record);
     var sync;
     if (this.descriptor.onchange) {
-        sync = this.descriptor.onchange.invoke(this, newRecord);
+        sync = this.descriptor.onchange.invoke(this, newRecord, this.getVariablesContext());
     }
 
     function update() {
@@ -217,6 +248,7 @@ ASHField.prototype.execOnChange = function () {
 };
 
 ASHField.prototype.notifyChange = function () {
+    this.pinFire('value');
     this.row.notifyPropertyChange(this.pName);
 };
 
@@ -298,22 +330,47 @@ Object.defineProperty(ASHField.prototype, 'value', {
         if ('calc' in this.descriptor) {
             return this.descriptor.calc;
         }
-        return this.row.record[this.pName];
+        var record = this.row.record;
+        if (this.isExtraField) {
+            if (record.extrainfo && (typeof record.extrainfo === 'object')) {
+                return record.extrainfo[this.pName];
+            }
+            else return undefined;
+        }
+        return record[this.pName];
     },
     set: function (value) {
         value = this.implicit(value);
         if (this.isNoneValue(value)) value = undefined;
-        var prevValue = this.row.record[this.pName];
+        var record = this.row.record;
+        var objCtn = record;
+        var prevValue;
+        if (this.isExtraField) {
+            if (!record.extrainfo || (typeof record.extrainfo !== 'object')) {
+                record.extrainfo = {};
+            }
+            objCtn = record.extrainfo;
+        }
+        prevValue = objCtn[this.pName];
 
         if (!this.isEqual(value, prevValue)) {
-            if (value === undefined) delete this.row.record[this.pName];
-            else this.row.record[this.pName] = value;
+            if (value === undefined) delete objCtn[this.pName];
+            else objCtn[this.pName] = value;
             this.loadValue();
             this.execOnChange();
             this.notifyChange();
         }
     }
 });
+
+ASHField.prototype.pinHandlers.value = {
+    get: function () {
+        return this.value;
+    },
+    receives: function (value) {
+        this.value = value;
+    }
+};
 
 /**
  * @augments EventEmitter
@@ -325,6 +382,7 @@ Object.defineProperty(ASHField.prototype, 'value', {
 export function ASHTRow(table, record) {
     EventEmitter.call(this);
     Context.apply(this);
+    this.attach(table);
     this.table = table;
     this.id = randomIdent(24);
     this.busy = false;
@@ -341,6 +399,10 @@ export function ASHTRow(table, record) {
     this.changedPNames = [];
 
     this.record = record;
+    this.lines = [];
+    this.blocks = [];
+    this.makeCircuit();
+
 }
 
 OOP.mixClass(ASHTRow, EventEmitter, Context);
@@ -357,11 +419,47 @@ ASHTRow.prototype.remove = function () {
     this.table.removeRow(this);
 };
 
+ASHTRow.prototype.makeCircuit = function () {
+    var linesSource = this.table && this.table.formSource && this.table.formSource.lines;
+    var blocksSource = this.table && this.table.formSource && this.table.formSource.blocks;
+    if (blocksSource) {
+        this.blocks = blocksSource.map((bs) => {
+            bs = Object.assign({}, bs);
+            if (bs.tag !== "Function") return null;//only support Function now
+            var block = new CBFunction();
+            block.attach(this);
+            delete bs.tag;
+            Object.assign(block.attributes, bs);
+            return block;
+        }).filter(x => !!x);
+    }
+    var blockDict = this.blocks.reduce((ac, cr) => {
+        if (cr.attributes.name) ac[cr.attributes.name] = cr;
+        ac[cr.id] = cr;
+        return ac;
+    }, {});
+
+    blockDict = this.properties.reduce((ac, cr) => {
+        ac[cr.pName] = cr;
+        if (cr.pId) ac[cr.pId] = cr;
+        return ac;
+    }, blockDict);
+
+    if (linesSource) {
+        this.lines = linesSource.map((ls) => {
+            var u = blockDict[ls.u];
+            var v = blockDict[ls.v];
+            if (!u || !v) return null;
+            return new CCLine(u, ls.uPin, v, ls.vPin);
+        }).filter(x => !!x);
+    }
+};
+
 ASHTRow.prototype.notifyPropertyChange = function (pName) {
     if (this.changedPNames.indexOf(pName) < 0) {
         this.changedPNames.push(pName);
         this.table.domSignal.emit(this.id + '_property_change');
-        this.emit('property_change', { target: this, record: this.record, pName: pName }, this);
+        this.emit('property_change', {target: this, record: this.record, pName: pName}, this);
     }
 };
 
@@ -458,12 +556,91 @@ export function ASHTTable(editor, opt) {
     EventEmitter.call(this);
     this.editor = editor;
     this.opt = opt || {};
+    this.formSource = null;
     this.propertyNames = [];
     this.propertyDescriptors = {};
+    this.attach(editor);
 }
 
 
 OOP.mixClass(ASHTTable, Context, EventEmitter);
+
+
+ASHTTable.prototype.form2Desc = {
+    TextInput: 'text',
+    DateInput: 'date'
+};
+
+ASHTTable.prototype.form2Desc.ComboBox = function (nd, descriptor) {
+    descriptor.type = 'ComboBox';
+    descriptor.items = nd.items;
+};
+
+
+ASHTTable.prototype.form2Desc.TreeComboBox = function (nd, descriptor) {
+    descriptor.type = 'TreeComboBox';
+    descriptor.items = nd.items;
+};
+
+
+ASHTTable.prototype.form2Desc.TreeLeafComboBox = function (nd, descriptor) {
+    descriptor.type = 'TreeLeafComboBox';
+    descriptor.items = nd.items;
+};
+
+
+/**
+ * from form to datasheet data
+ */
+ASHTTable.prototype.computeFormSource = function () {
+    if (!this.formSource) return;
+    var propertyNames = [];
+    var propertyDescriptors = {};
+
+    var labels = {};//by id
+    var visit = nd => {
+        var name;
+        var displayName;
+        var descriptor;
+        if (nd.children && nd.children.length > 0) {
+            nd.children.forEach(visit);
+            return;
+        }
+        if (nd.tag === 'Container') return;
+        if (nd.tag === 'Label') {
+            if (nd.id) labels[nd.id] = nd;
+        }
+        else {
+            if (!nd.name) return;// not a property
+            descriptor = {};
+            propertyDescriptors[nd.name] = descriptor;
+            propertyNames.push(nd.name);
+            descriptor.text = (labels[nd.id + '_label'] && labels[nd.id + '_label'].value) || nd.name;
+            descriptor.pId = nd.id;
+            if (nd.readOnly) descriptor.readOnly = true;
+            if (nd.outputMode) descriptor.readOnly = true;
+            if (nd.isExtraField) descriptor.isExtraField = true;
+            if ("value" in nd) {
+                descriptor.defaultValue = nd.value;
+            }
+
+            var cvt = this.form2Desc[nd.tag] || (TDBase.typeClasses[nd.tag] ? nd.tag : null);
+            if (typeof cvt === "string") {
+                descriptor.type = cvt;
+
+            }
+            else if (typeof cvt === "function") {
+                cvt(nd, descriptor);
+            }
+            else {
+                descriptor.type = 'text';
+            }
+        }
+    }
+    visit(this.formSource.layout);
+    this.propertyNames = propertyNames;
+    this.propertyDescriptors = propertyDescriptors;
+};
 
 ASHTTable.prototype.computeHeader = function () {
     var propertyNames = this.propertyNames;
@@ -472,8 +649,10 @@ ASHTTable.prototype.computeHeader = function () {
 };
 
 ASHTTable.prototype.import = function (data) {
+    this.formSource = data.formSource;
     this.propertyNames = data.propertyNames;
     this.propertyDescriptors = data.propertyDescriptors;
+    this.computeFormSource();
     this.computeHeader();
 };
 
@@ -511,13 +690,40 @@ ASHTTable.prototype.rowIndexOf = function (row) {
     throw new Error("Not Implement!");
 }
 
-ASHTTable.prototype.rowAt = function (idx){
+ASHTTable.prototype.rowAt = function (idx) {
     throw new Error("Not Implement!");
 };
 
 
 ASHTTable.prototype.getHash = function () {
     throw new Error("Not Implement!");
+};
+
+ASHTTable.prototype.getVariablesContext = function () {
+    //variableScope
+    var context = {};
+    var variableScope;
+
+    if (this.fragment) {
+        /**
+         * @type {SCScope}
+         */
+        variableScope = this.fragment.getContext('variableScope');
+        if (variableScope) {
+            variableScope = variableScope.makeFlattenedScope();
+            Object.keys(variableScope.data).forEach(key => {
+                context[key] = variableScope.data[key];
+            });
+        }
+    }
+    variableScope = this.getContext('variableScope');
+    if (variableScope) {
+        variableScope = variableScope.makeFlattenedScope();
+        Object.keys(variableScope.data).forEach(key => {
+            context[key] = variableScope.get(key);
+        });
+    }
+    return context;
 };
 
 /**
