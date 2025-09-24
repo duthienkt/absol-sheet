44b91723a74700457fda56dca88914fb0523c748
duthienkt <blueskythien2010@live.com>
2025-09-24 08:08:15 +0700

    - default value

--- js/viewer/types/TDText.js
+++ js/viewer/types/TDText.js
@@ -1,6 +1,6 @@
 import TDBase from "./TDBase";
 import OOP from "absol/src/HTML5/OOP";
-import {_} from "../../dom/SCore";
+import { _ } from "../../dom/SCore";
 
 
 /***
@@ -9,6 +9,11 @@ import {_} from "../../dom/SCore";
  */
 function TDText() {
     TDBase.apply(this, arguments);
+    this.elt.addClass('asht-type-text');
+    //Apr04-2025; Yen required to default value must be empty string
+    if (this.row.record[this.pName] === undefined || this.row.record[this.pName] === null) {
+        this.row.record[this.pName] = '';
+    }
 }
 
 OOP.mixClass(TDText, TDBase);
@@ -16,6 +21,7 @@ OOP.mixClass(TDText, TDBase);
 TDText.prototype.implicit = function (value) {
     if (value === undefined || value === null) return null;
     if (typeof value === "object") return JSON.stringify(value);
+    if (typeof value === "number") return value.toString();
     if (typeof value !== "string") return '';
     return value + '';
 };
@@ -26,14 +32,21 @@ TDText.prototype.loadValue = function () {
     this.$lines = value.split(/\r?\n/).reduce(function (ac, line) {
         line = line.replace(/\s\s/g, ' \u00A0');
         ac.push(_({
-            tag: 'span', child: {text: line}
+            tag: 'span', child: { text: line }
         }));
         ac.push(_('br'));
         return ac;
     }, []);
     this.elt.addChild(this.$lines);
+    if (this.$lines.length > 1) {
+        this.elt.removeClass('as-multi-line');
+    }
+    else {
+        this.elt.addClass('as-multi-line');
+    }
 };
 
 
 TDBase.typeClasses.text = TDText;
+TDBase.typeClasses.TextInput = TDText;
 export default TDText;
